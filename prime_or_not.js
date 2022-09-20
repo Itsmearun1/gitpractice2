cc68dda6c82a338a1ab9a7519ea8be12443ed77d
Itsmearun1 <arundevvb1@gmail.com>
2022-09-20 21:45:40 +0530

    prime program

---
+++ prime_or_not.js
@@ -0,0 +1,12 @@
+function prime_check(num){
+     
+    for(let i=2;i>=num**0.5;i++){
+    if(num%i == 0){
+        return "Not Prime";
+    }
+    }
+   
+        return "Prime"; 
+}
+
+console.log(prime_check(0)); 
