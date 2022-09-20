12d3af261a7d3de157f413decf69dd33cd013adc
Itsmearun1 <arundevvb1@gmail.com>
2022-09-20 21:58:23 +0530

    m2 prime fix

--- prime_or_not.js
+++ prime_or_not.js
@@ -1,12 +1,14 @@
 function prime_check(num){
-     
-    for(let i=2;i>=num**0.5;i++){
-    if(num%i == 0){
-        return "Not Prime";
-    }
-    }
-   
-        return "Prime"; 
-}
-
-console.log(prime_check(0)); 
+    if(num<2 || num==undefined){
+     return "Not Prime";
+    } 
+     for(let i=2;i<=num**0.5;i++){
+     if(num%i == 0){
+         return "Not Prime";
+     }
+     }
+    
+         return "Prime"; 
+ }
+ 
+ console.log(prime_check(0));
